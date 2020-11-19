8dbed8af9a1126dd936a62baffef12e11c1c54bb
woshiamiaojiang <woshiamiaojiang@163.com>
2020-11-19 23:44:52 +0800

    add

--- Week_05/week5/src/main/java/com/geekbang/homework6/JdbcPreparedStatement.java
+++ Week_05/week5/src/main/java/com/geekbang/homework6/JdbcPreparedStatement.java
@@ -7,7 +7,7 @@ public class JdbcPreparedStatement {
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost:3306/test";
     static final String USER = "root";
-    static final String PASS = "123456";
+    static final String PASS = "root";
 
     public static void main(String[] args) {
         try {
